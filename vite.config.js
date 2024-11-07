714dda09aff24e83ee85fa8790183e8cb1ed5533
Christian Espindola <christian.espindola@hotmail.com>
2024-11-07 13:46:02 -0300

    actualizando detalles para netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:`https://ChristianEspindola.github.io/Porfolio`,
-})
+  base: "./", // Esto asegura que los recursos se busquen en la raíz relativa
+  build: {
+    outDir: "dist", // Directorio donde se guardará la compilación final
+  },
+});
