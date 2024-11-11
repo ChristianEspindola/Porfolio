1b87fb73a8d654e728434f165b59f04da564f335
Christian Espindola <christian.espindola@hotmail.com>
2024-11-11 14:22:50 -0300

    se configura form prueba 5

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useNetlifyForm, NetlifyFormProvider } from "react-netlify-forms";
 import "./ContactForm.css";
 
 const ContactForm = () => {
@@ -17,74 +16,65 @@ const ContactForm = () => {
     }));
   };
 
-  const { handleSubmit, success, error } = useNetlifyForm({
-    name: "contact-form",
-  });
-
   return (
-    <NetlifyFormProvider>
-      <div
-        id="contacto"
-        className="contact-form"
-        data-aos="flip-left"
-        data-aos-easing="ease-out-cubic"
+    <div
+      id="contacto"
+      className="contact-form"
+      data-aos="flip-left"
+      data-aos-easing="ease-out-cubic"
+    >
+      <h3>Contáctame</h3>
+
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
       >
-        <h3>Contáctame</h3>
-        <form
-          name="contact-form"
-          method="POST"
-          data-netlify="true"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contact-form" />
-          <div className="form-group">
-            <label htmlFor="name">Nombre</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Correo Electrónico</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Mensaje</label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-          </div>
-          <div className="containerbutton">
-            <button className="button" type="submit">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span className="text">Enviar</span>
-            </button>
-          </div>
-          {success && <p>¡Gracias por tu mensaje!</p>}
-          {error && (
-            <p>Hubo un error al enviar el mensaje. Inténtalo de nuevo.</p>
-          )}
-        </form>
-      </div>
-    </NetlifyFormProvider>
+        <input type="hidden" name="form-name" value="contact" />
+        <div className="form-group">
+          <label htmlFor="name">Nombre</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Correo Electrónico</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Mensaje</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <div className="containerbutton">
+          <button className="button" type="submit">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span className="text">Enviar</span>
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
